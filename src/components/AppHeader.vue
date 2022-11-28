b2c3b8b8088ca3fd6e29eed9a416d0df30c5829f
Therakar <matteo.pau200@gmail.com>
2022-11-28 20:32:17 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+  name: "AppHeader"
+}
+
+</script>
+
+<template>
+  <div>
+    HEADER
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+    export default{
+  name: "AppMain"
+}
+</script>
+
+<template>
+    <div>
+        MAIN
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
